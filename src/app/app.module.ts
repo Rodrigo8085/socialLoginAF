795c65addbb0f096b0c06d1cb5579c79be93706f
Rodrigo Santiago <rodrigo@consucorp.com.mx>
2022-10-17 00:29:42 -0500

    Test import and apply lib button alert library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ButtonAlertModule } from "@rodrigo8085/button-alert";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
