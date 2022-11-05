0ee3f72195f173458fce9d086669014c371c57d9
Rodrigo Santiago <rodrigo@consucorp.com.mx>
2022-11-05 09:17:21 -0600

    Complete and install llibrary from git repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public mainForm: FormGroup = this.fb.group({});
+
+  constructor(
+    private fb: FormBuilder) {
+      
+    }
+
+  ngOnInit(): void {
+    const controls: { [key: string]: FormControl } = {};
+
+    const validatorsToAdd = [];
+    validatorsToAdd.push(Validators.required);
+    // validatorsToAdd.push(Validators.pattern(/^\S/));
+    validatorsToAdd.push(Validators.pattern(/^[a-zA-ZáéíóúÁÉÍÓÚñÑÀàÂâÊêÎîöÛûÜüÇçÃãÕõ]+(?: [a-zA-ZáéíóúÁÉÍÓÚñÑÀàÂâÊêÎîöÛûÜüÇçÃãÕõ]+)*$/))
+    // validatorsToAdd.push(Validators.pattern(/(\S{2,})/));
+
+
+    controls['nombreValidador'] = this.fb.control(
+      null,
+      validatorsToAdd
+  );
+  this.mainForm = this.fb.group(controls);
+    console.log(this.mainForm);
+
+
+    // this.mainForm.addControl('nombreValidador', validatorsToAdd);
+    this.valuesChanges();
+  }
+
+  valuesChanges(){
+    this.mainForm.valueChanges.subscribe(t => {
+      console.log(this.mainForm.get('nombreValidador'))
+    })
+  }
+
+  onSubmit(){
+
+  }
   title = 'socialLoginAF';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonAlertModule } from "@rodrigo8085/button-alert";
+// import { ButtonAlertModule } from "@rodrigo8085/button-alert";
+// import { ButtonAlertModule } from "test-libreria/projects/button-alert/src/lib/button-alert.module"
+// import { ButtonAlertModule } from "test-libreria/projects/button-alert/src/public-api";
+import { ButtonAlertModule } from '@rodrigo8085/button-alert';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,7 +16,9 @@ import { ButtonAlertModule } from "@rodrigo8085/button-alert";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonAlertModule
+    ButtonAlertModule,
+    CommonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
